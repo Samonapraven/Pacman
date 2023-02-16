d5d81141083e74de7ad7ce1a1284929d0a016f3a
PETAR <pvp2004@abv.bg>
2023-02-16 22:01:42 +0200

    isolated the map from the spritesheet

---
+++ index.js
@@ -0,0 +1,38 @@
+let canvas = document.querySelector('canvas')
+let context = canvas.getContext('2d')
+
+let image = new Image()
+image.src = 'spritesheet.png'
+image.crossOrigin = true;
+
+image.onload = () => {
+    canvas.width = 680;
+    canvas.height = 248;
+    
+    context.drawImage(
+        image,
+        -450, 
+        0, // whole spritesheet x and y offset positions
+        image.width,
+        image.height, // width and height of the source image
+        0,
+        0, // desired offset to isolate the map
+        canvas.width,
+        canvas.height
+    )
+}
+
+// image.onload = () => {
+//     context.drawImage(
+//         image,
+//         0,
+//         0,
+//         image.width,
+//         image.height,
+//         0,
+//         0,
+//         canvas.width,
+//         canvas.height
+//     )
+// }
+
